64a16ee35969ea87320c8f4ef5cf701fb095f7be
HyunjaePark <hyunjae@HyunjaeParkui-MacBookPro.local>
2020-12-28 14:51:04 +0900

    [settings] react native settings

---
+++ constants/icons.js
@@ -0,0 +1,7 @@
+export const search = require("../assets/icons/");
+export const menu = require("../assets/icons/")
+
+export default {
+  search,
+  menu,
+}
---
+++ constants/images.js
@@ -0,0 +1,13 @@
+export const nike1 = require("../assets/images/alex.png");
+export const nike2 = require("../assets/images/imani.png");
+export const nike3 = require("../assets/images/jaredd.png");
+export const nike4 = require("../assets/images/josh.png");
+export const nike5 = require("../assets/images/regis.png");
+
+export default {
+  nike1,
+  nike2,
+  nike3,
+  nike4,
+  nike5,
+}
